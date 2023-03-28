b3463e81234e70a3b78424157d32f58bd019b449
sebasGZA <sebassga34@gmail.com>
2023-03-28 11:27:52 -0500

    Config MUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { JournalApp } from "./JournalApp";
 import "./styles.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
