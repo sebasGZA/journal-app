18622bffb21eda66013e6b9232b42fdd56a0711f
sebasGZA <sebassga34@gmail.com>
2023-03-28 20:39:29 -0500

    Float button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layouts/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
@@ -7,6 +9,21 @@ export const JournalPage = () => {
       {/* <NothingSelectedView /> */}
 
       <NoteView />
+      
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined />
+      </IconButton>
+
     </JournalLayout>
   );
 };
